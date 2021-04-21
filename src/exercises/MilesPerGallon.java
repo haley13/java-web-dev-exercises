09e2d4c7d055e654eccec81bee285d74cb32bfb6
haley13 <allickhaley@gmail.com>
2021-04-21 13:34:55 -0500

    completed exercises 1-4

---
+++ src/exercises/MilesPerGallon.java
@@ -0,0 +1,17 @@
+package exercises;
+
+import java.util.Scanner;
+
+public class MilesPerGallon {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number of miles driven: ");
+        double miles= input.nextInt();
+        System.out.println("Enter gallons of gas used: ");
+        double gallons= input.nextInt();
+        double mpg= miles / gallons;
+
+        System.out.println("Your distance in miles  "    + miles + " and gallons of gas "  + gallons +  " equals " + mpg + " miles per gallon");
+
+    }
+}
