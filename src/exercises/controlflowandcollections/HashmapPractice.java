e32223fc1c9213bd08709ae50627440e6fca40a4
haley13 <allickhaley@gmail.com>
2021-04-22 21:20:48 -0500

    finished HashmapPractice

--- src/exercises/controlflowandcollections/HashmapPractice.java
+++ src/exercises/controlflowandcollections/HashmapPractice.java
@@ -1,4 +1,35 @@
 package exercises.controlflowandcollections;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 public class HashmapPractice {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        HashMap<Integer, String> students = new HashMap<>();
+        String newStudent;
+        System.out.println("Enter your students (or ENTER to finish):");
+
+        do {
+
+            System.out.print("Student: ");
+            newStudent = input.nextLine();
+
+            if (!newStudent.equals("")) {
+                System.out.print("Student ID: ");
+                Integer studentId = input.nextInt();
+                students.put(studentId, newStudent);
+
+            }
+        } while (!newStudent.equals(""));
+
+        System.out.println("\nClass roster:");
+
+        for (Map.Entry<Integer, String> student : students.entrySet()) {
+            System.out.println(student.getKey() + " (" + student.getValue() + ")");
+
+        }
+
+
+    }
 }
