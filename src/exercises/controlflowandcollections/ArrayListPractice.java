7a320b84c4a72f403c46fde0fd649e0cbda91350
haley13 <allickhaley@gmail.com>
2021-04-22 21:27:34 -0500

    finished ArrayListPractice

--- src/exercises/controlflowandcollections/ArrayListPractice.java
+++ src/exercises/controlflowandcollections/ArrayListPractice.java
@@ -3,11 +3,24 @@ package exercises.controlflowandcollections;
 
 
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 
 public class ArrayListPractice {
     public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(6);
+        numbers.add(7);
+        numbers.add(8);
+        numbers.add(9);
+        numbers.add(10);
+        System.out.println(sumEven(numbers));
     }
 
     public static int sumEven(ArrayList<Integer> arr) {
@@ -16,22 +29,13 @@ public class ArrayListPractice {
             if (integer % 2 == 0)
                 total += integer;
         }
-
         return total;
+
     }
+
 }
 
-//        ArrayList<Integer> numbers = new ArrayList<>();
-//        numbers.add(1);
-//        numbers.add(2);
-//        numbers.add(3);
-//        numbers.add(4);
-//        numbers.add(5);
-//        numbers.add(6);
-//        numbers.add(7);
-//        numbers.add(8);
-//        numbers.add(9);
-//        numbers.add(10);
+//
 //
 //      int total = addNumbers.(numbers);
 //
