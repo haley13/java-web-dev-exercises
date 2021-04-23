572e97d02ba1363ad9cfa6d347d97a9f293b704e
haley13 <allickhaley@gmail.com>
2021-04-22 20:05:21 -0500

    studio countingCharacters finished

--- src/exercises/ArrayListPractice.java
+++ src/exercises/ArrayListPractice.java
@@ -1,16 +1,39 @@
 package exercises;
 
-import java.util.Scanner;
+
+
+
 import java.util.ArrayList;
+
 
 public class ArrayListPractice {
     public static void main(String[] args) {
-        ArrayList<Double> numbers = new ArrayList<>();
-        Scanner input = new Scanner(System.in);
-
-
-
     }
 
+    public static int sumEven(ArrayList<Integer> arr) {
+        int total = 0;
+        for (int integer : arr) {
+            if (integer % 2 == 0)
+                total += integer;
+        }
+
+        return total;
+    }
 }
+
+//        ArrayList<Integer> numbers = new ArrayList<>();
+//        numbers.add(1);
+//        numbers.add(2);
+//        numbers.add(3);
+//        numbers.add(4);
+//        numbers.add(5);
+//        numbers.add(6);
+//        numbers.add(7);
+//        numbers.add(8);
+//        numbers.add(9);
+//        numbers.add(10);
+//
+//      int total = addNumbers.(numbers);
+//
+
 
---
+++ src/exercises/StaticMethodArrayList.java
@@ -0,0 +1,7 @@
+package exercises;
+
+public class StaticMethodArrayList {
+    Integer addNumbers(Integer numbers) {
+        return addNumbers(numbers);
+    }
+}
